ef5910f6e33badcad67d4d3a7295638df72421c8
kalkedan eyerusalem <kalkedan.eyerusalem@obi.org>
2026-04-14 18:38:27 -0500

    more commit

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.PORT || 3000;
+  const port = Number(process.env.PORT) || 3000;
   await app.listen(port);
 }
 bootstrap();
